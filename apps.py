5f2f9ae3a1e47e6a56d0cb462902f282f7873ac7
go19kul16 <117854447+go19kul16@users.noreply.github.com>
2023-07-11 01:04:12 +0530

    Create apps.py

---
+++ apps.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="GRADE CALCULATOR ",page_icon="✅",layout="wide")
+st.title(":green[GRADE] :blue[CALCULATOR]")
+col1,col3=st.columns(2)
+df=pd.read_excel("GRADE.xlsx",index_col=0,engine="openpyxl")
+   
+with col1:
+    internal=0
+    d=0
+    internal=st.number_input("ENTER YOUR INTERNAL MARK:",step=None,min_value=0,max_value=50)
+    inte=int(float(internal))
+    if st.button("Submit"):
+        if(inte>50):
+            st.subheader("ENTER MARK :red[ < 50]")
+        d=inte
+        df1=df.loc[d]
+        st.table(df1)
+        
+    st.subheader("TO SEE IN :blue[BAR GRAPH]")
+    st.markdown("Press the :red[Display] Button")
+    if st.button("DISPLAY"):
+        st.markdown(":white[Note:]:red[Bars Are arranged in Ascending Order]")
+        d=inte
+        df1=df.loc[d]
+        col_name=list(df.columns)
+        st.bar_chart(df1,x=col_name)
