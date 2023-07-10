4f9566afb27e893aa147b33309a7e21ca093923e
go19kul16 <117854447+go19kul16@users.noreply.github.com>
2023-07-10 14:00:43 +0530

    app1.py

--- app1.py
+++ app1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import altair as alt
+import openpyxl
 
 st.set_page_config(page_title="GRADE CALCULATOR ",page_icon="✅",layout="wide")
 st.title(":green[GRADE] :blue[CALCULATOR]")
